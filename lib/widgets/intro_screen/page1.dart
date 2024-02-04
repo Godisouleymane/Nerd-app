39f794f91923c990bde1b48a38aea2858481c4b4
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-04 15:40:48 +0000

    ajout de l'image sur l'ecran 1

--- lib/widgets/intro_screen/page1.dart
+++ lib/widgets/intro_screen/page1.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class introPage1 extends StatelessWidget {
   const introPage1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text('Page 1'),
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Container(
+            child: SvgPicture.asset(
+            "assets/programmer-animate.svg",
+            ),
+          ),
+        )
+      ],
     );
   }
 }
