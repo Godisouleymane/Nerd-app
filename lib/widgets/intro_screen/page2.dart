c0b0ec6af9265d03f65d98a82781f2865038156e
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-04 16:31:21 +0000

    ajout de l'image, texte, sur l'cran 2

--- lib/widgets/intro_screen/page2.dart
+++ lib/widgets/intro_screen/page2.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class introPage2 extends StatelessWidget {
   const introPage2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-       child: Center(
-        child: Text('Page 2'),
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
+        children: [
+          Container(
+            child: SvgPicture.asset(
+              "assets/Hand coding-amico.svg",
+              width: 300,
+              height: 300,
+            ),
+          ),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Apprentissage Pratique",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Plongez dans des projets pratiques et renforcez vos compétences avec des exercices interactifs. Codez en temps réel et consolidez vos connaissances.",
+            style: TextStyle(fontSize: 15, color: Colors.grey),
+          )
+        ],
       ),
     );
   }
