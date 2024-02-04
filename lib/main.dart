0d82bc2f5469138a6515c34a069c50207ea240cc
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-04 18:44:04 +0000

    class AuthService pour se connecter avec google

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:code_crafters/firebase_options.dart';
 import 'package:code_crafters/widgets/onBoarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'CodeCrafters',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/services/authentification.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // connexion avec google,
+  Future<UserCredential> signInWithGoogle() async {
+    // declancher le flux d'authentification
+
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    // obtenir les details d'autorisation de la demande
+
+    final googleAuth = await googleUser!.authentication;
+
+    // creer un nouvel identifiant
+
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    // une fois connecter renvoyer l'identifiant de l'utilisateur;
+
+    return await _auth.signInWithCredential(credential);
+  }
+
+  // L'etat de l'utilisateur en temps reel
+
+  Stream<User?> get user => _auth.authStateChanges();
+}
