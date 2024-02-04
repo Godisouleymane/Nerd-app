0b4035fcc58304cfed5728205d72d3acc6eeec38
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-04 16:33:35 +0000

    ajout de l'image, texte, sur l'cran 3

--- lib/widgets/intro_screen/page3.dart
+++ lib/widgets/intro_screen/page3.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class introPage3 extends StatelessWidget {
   const introPage3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-       child: Center(
-        child: Text('Page 3'),
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
+        children: [
+          Container(
+            child: SvgPicture.asset(
+              "assets/Achievement-amico.svg",
+              width: 300,
+              height: 300,
+            ),
+          ),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Progressez et Maîtrisez",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Suivez votre progression, remportez des succès et maîtrisez de nouvelles compétences. Codez avec confiance et atteignez vos objectifs de développement.",
+            style: TextStyle(fontSize: 15, color: Colors.grey),
+          )
+        ],
       ),
     );
   }
