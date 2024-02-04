b270145fc33f7f6224aa91541192c038f5fe94cc
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-04 23:17:35 +0000

    AppBar

--- lib/choixParcours/introduction.dart
+++ lib/choixParcours/introduction.dart
@@ -8,12 +8,32 @@ class IntroductionPage extends StatefulWidget {
 }
 
 class _IntroductionPageState extends State<IntroductionPage> {
+  String selectedContainerId = "";
+  Color defaultBorderColor = Colors.transparent;
+  Color selectedBorderColor = Colors.green;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Introduction page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            "CodeCrafters",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 53, 32, 149),
+        ),
+        backgroundColor: const Color.fromARGB(255, 27, 16, 74),
+        body: const Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Text("Choisissez ce que vous voullez apprendre", style: TextStyle(
+                color: Colors.white,
+                fontSize: 25
+              ),)
+              ],
+          ),
+        ));
   }
 }
