447b53d29934ea7f7c6cba36b77e12a4911ee604
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-05 18:20:06 +0000

    positionner le button continuer tout en bas en Stack

--- lib/choixParcours/introduction.dart
+++ lib/choixParcours/introduction.dart
@@ -24,93 +24,100 @@ class _IntroductionPageState extends State<IntroductionPage> {
           backgroundColor: const Color.fromARGB(255, 53, 32, 149),
         ),
         backgroundColor: const Color.fromARGB(255, 27, 16, 74),
-        body: ListView(
+        body: Stack(
           children: [
-            const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text(
-                "Choisissez ce que vous voulez apprendre",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: Text(
-                  "Développement web :",
-                  style: TextStyle(
-                      color: Colors.orange,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 20),
-                  textAlign: TextAlign.left,
+            ListView(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Text(
+                    "Choisissez ce que vous voulez apprendre",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
+                const Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "Vous pouvez changer ça plus tard",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 300,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      CustomGestureDetector(
+                        containerId: 'html',
+                        defaultBorderColor: Colors.transparent,
+                        selectedBorderColor: Colors.orange,
+                        onTapCallback: updateSelectedContainer,
+                        containerIcon: Icon(
+                          DevIcons.html5PlainWordmark,
+                          size: 100,
+                          color: Colors.orange[700],
+                        ),
+                        isSelected: selectedContainerId == 'html',
+                        autreInformation: 'HTML',
+                        autreVariable:
+                            '* Construisez des bases solides pour vos sites web en apprenant HTML',
+                      ),
+                      CustomGestureDetector(
+                        containerId: 'css',
+                        defaultBorderColor: Colors.transparent,
+                        selectedBorderColor: Colors.blue,
+                        onTapCallback: updateSelectedContainer,
+                        containerIcon: const Icon(
+                          DevIcons.css3PlainWordmark,
+                          size: 100,
+                          color: Colors.blue,
+                        ),
+                        isSelected: selectedContainerId == 'css',
+                        autreInformation: 'CSS',
+                        autreVariable:
+                            '* Donnez du style à vos sites web avec CSS',
+                      ),
+                      CustomGestureDetector(
+                        containerId: 'javaScript',
+                        defaultBorderColor: Colors.transparent,
+                        selectedBorderColor: Colors.amber,
+                        onTapCallback: updateSelectedContainer,
+                        containerIcon: const Icon(
+                          DevIcons.javascriptPlain,
+                          size: 100,
+                          color: Colors.amber,
+                        ),
+                        isSelected: selectedContainerId == 'javaScript',
+                        autreInformation: 'JavaScript',
+                        autreVariable:
+                            "* Explorez l'interactivité avec JavaScript. Manipulez le contenu dynamique, gérez les événements ",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: 300,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                   CustomGestureDetector(
-                    containerId: 'html',
-                    defaultBorderColor: Colors.transparent,
-                    selectedBorderColor: Colors.green,
-                    onTapCallback: updateSelectedContainer,
-                    containerIcon: Icon(
-                      DevIcons.html5PlainWordmark,
-                      size: 100,
-                      color: Colors.orange[700],
-                    ),
-                    isSelected: selectedContainerId == 'html',
-                    autreInformation: 'HTML',
-                    autreVariable: '* Construisez des bases solides pour vos sites web en apprenant HTML',
-                   
-                  ),
-                  CustomGestureDetector(
-                    containerId: 'css',
-                    defaultBorderColor: Colors.transparent,
-                    selectedBorderColor: Colors.green,
-                    onTapCallback: updateSelectedContainer,
-                    containerIcon: const Icon(
-                      DevIcons.css3PlainWordmark,
-                      size: 100,
-                      color: Colors.blue,
-                    ),
-                    isSelected: selectedContainerId == 'css',
-                    autreInformation: 'CSS',
-                    autreVariable: '* Donnez du style à vos sites web avec CSS',
-                  ),
-                  CustomGestureDetector(
-                    containerId: 'javaScript',
-                    defaultBorderColor: Colors.transparent,
-                    selectedBorderColor: Colors.green,
-                    onTapCallback: updateSelectedContainer,
-                    containerIcon: const Icon(
-                      DevIcons.javascriptPlain,
-                      size: 100,
-                      color: Colors.amber,
-                    ),
-                    isSelected: selectedContainerId == 'javaScript',
-                    autreInformation: 'JavaScript',
-                    autreVariable: "* Explorez l'interactivité avec JavaScript. Manipulez le contenu dynamique, gérez les événements ",
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child:
-                  ElevatedButton(onPressed: () {}, child: Text("Continuer")),
-            )
+            Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 10.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: Text("Continuer")),
+                ))
           ],
         ));
   }
