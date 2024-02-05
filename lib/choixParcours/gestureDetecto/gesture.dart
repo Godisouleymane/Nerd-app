1ed0de92d075da60b43d3eef75fac157f955b15d
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-05 01:44:09 +0000

    pouvoirselectionner un cours

--- lib/choixParcours/gestureDetecto/gesture.dart
+++ lib/choixParcours/gestureDetecto/gesture.dart
@@ -6,6 +6,9 @@ class CustomGestureDetector extends StatefulWidget {
   final Color selectedBorderColor;
   final Function(String) onTapCallback;
   final Icon? containerIcon;
+  final String autreInformation;
+  final String autreVariable;
+  final bool isSelected; // Ajout de la variable isSelected
 
   const CustomGestureDetector({
     required this.containerId,
@@ -13,15 +16,20 @@ class CustomGestureDetector extends StatefulWidget {
     required this.selectedBorderColor,
     required this.onTapCallback,
     this.containerIcon,
-  });
-
+    required this.autreInformation,
+    required this.autreVariable,
+    this.isSelected = false, // Initialisation de la variable isSelected
+    Key? key,
+  }) : super(key: key);
   @override
   _CustomGestureDetectorState createState() => _CustomGestureDetectorState();
 }
 
 class _CustomGestureDetectorState extends State<CustomGestureDetector> {
+   bool isSelected = false;
   @override
   Widget build(BuildContext context) {
+   
     return GestureDetector(
       onTap: () {
         widget.onTapCallback(widget.containerId);
@@ -30,21 +38,33 @@ class _CustomGestureDetectorState extends State<CustomGestureDetector> {
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
+          color: Colors.blue,
           border: Border.all(
-            color: widget.containerId == widget.containerId
+            color: widget.isSelected
                 ? widget.selectedBorderColor
                 : widget.defaultBorderColor,
             width: 2.0,
           ),
         ),
-        child: Column(
-          children: [
-            if (widget.containerIcon != null) widget.containerIcon!,
-            Text(
-              'Container ${widget.containerId}',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.containerIcon != null) widget.containerIcon!,
+              Text(
+                'Container ${widget.containerId}',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                'Autre Information : ${widget.autreInformation}',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                'Autre Variable : ${widget.autreVariable}',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/choixParcours/introduction.dart
+++ lib/choixParcours/introduction.dart
@@ -1,4 +1,5 @@
 import 'package:code_crafters/choixParcours/gestureDetecto/gesture.dart';
+import 'package:dev_icons/dev_icons.dart';
 import 'package:flutter/material.dart';
 
 class IntroductionPage extends StatefulWidget {
@@ -9,8 +10,8 @@ class IntroductionPage extends StatefulWidget {
 }
 
 class _IntroductionPageState extends State<IntroductionPage> {
- 
-
+  String selectedContainerId = '';
+  List<String> selectedContainerIds = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,32 +29,63 @@ class _IntroductionPageState extends State<IntroductionPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text("Choisissez ce que vous voullez apprendre", style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold
+              const Text(
+                "Choisissez ce que vous voullez apprendre",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
+              SizedBox(
+                height: 30.0,
               ),
-              SizedBox(height: 30.0,),
               const Align(
                 alignment: Alignment.topLeft,
-                child: Text("Nos Cours :", style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 18
-                ),textAlign: TextAlign.left,),
+                child: Text(
+                  "Nos Cours :",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 18),
+                  textAlign: TextAlign.left,
+                ),
               ),
-              GesturePage(idContainer : ""),
+              CustomGestureDetector(
+                containerId: 'fullStack1',
+                defaultBorderColor: Colors.transparent,
+                selectedBorderColor: Colors.green,
+                onTapCallback: updateSelectedContainer,
+                containerIcon: Icon(Icons.code),
+                isSelected: selectedContainerId == 'fullStack1',
+                autreInformation: 'Description du conteneur 1',
+                autreVariable: 'Valeur 1',
+              ),
+              CustomGestureDetector(
+                containerId: 'fullStack2',
+                defaultBorderColor: Colors.transparent,
+                selectedBorderColor: Colors.green,
+                onTapCallback: updateSelectedContainer,
+                containerIcon: Icon(Icons.layers),
+                isSelected: selectedContainerId == 'fullStack2',
+                autreInformation: 'Description du conteneur 2',
+                autreVariable: 'Valeur 2',
+              ),
               Align(
                 alignment: Alignment.bottomCenter,
-                child: ElevatedButton(
-                  onPressed: (){}, 
-                  child: Text("Continuer")),
+                child:
+                    ElevatedButton(onPressed: () {}, child: Text("Continuer")),
               )
-              ],
+            ],
           ),
         ));
+  }
+
+  void updateSelectedContainer(String id) {
+    setState(() {
+      selectedContainerId = id;
+      print(id);
+    });
   }
 }
