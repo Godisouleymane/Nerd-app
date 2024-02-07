f9c13c9f224610a22c24a4dd2ba98264244e61ab
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-07 05:12:05 +0000

    creation du page html

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:code_crafters/services/course_selection_notifier.dart';
 import 'package:code_crafters/views/choixParcours/introduction.dart';
 import 'package:code_crafters/views/parcoursViews/html.dart';
 import 'package:code_crafters/views/widgets/onBoarding_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
@@ -11,15 +12,14 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _user = Provider.of<User?>(context);
+    final courseNotifier = Provider.of<CourseSelectionNotifier>(context);
 
-    final selectedCourse = CourseSelection().selectedCourse;
+    final selectedCourse = courseNotifier.selectedCourse;
 
-    print(
-        'Selected course: $selectedCourse'); // Ajoutez cette ligne pour vérifier la valeur de selectedCourse
+    print('Selected course: $selectedCourse');
 
     if (_user != null && selectedCourse != null) {
-      // Redirigez vers la page du cours sélectionné
-      switch (selectedCourse) {
+      switch (selectedCourse.id) {
         case 'html':
           return HTMLView();
         default:
