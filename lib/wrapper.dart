f95c447cac04a4996a069a7c399db9e685f67005
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-07 16:46:38 +0000

    creation du provider id container

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,20 +1,28 @@
+import 'package:code_crafters/provider/containerIdProvider.dart';
 import 'package:code_crafters/views/parcoursViews/html.dart';
 import 'package:code_crafters/views/widgets/onBoarding_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class Wrapper extends StatelessWidget {
-  const Wrapper({super.key});
+  const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final _user = Provider.of<User?>(context);
+    final selectedContainerId =
+        Provider.of<SelectedContainerIdProvider>(context).selectedContainerId;
+
     if (_user != null) {
-      return const HTMLView();
+      if (selectedContainerId == 'html') {
+        return HTMLView();
+      } else {
+        return Placeholder();
+      }
     } else {
       return OnBoardingScreen();
     }
   }
 }
+
