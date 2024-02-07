55ace7ba4187e6cffabf9fba61e433ef7b7c8958
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-07 02:45:10 +0000

    creation du bottomNavigation

---
+++ lib/views/parcoursViews/html.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HTMLView extends StatefulWidget {
+  const HTMLView({super.key});
+
+  @override
+  State<HTMLView> createState() => _HTMLViewState();
+}
+
+class _HTMLViewState extends State<HTMLView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/views/parcoursViews/shared-ui/bottomNaviagtionBar.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class BottomNaviBar extends StatefulWidget {
+  const BottomNaviBar({super.key});
+
+  @override
+  State<BottomNaviBar> createState() => _BottomNaviBarState();
+}
+
+class _BottomNaviBarState extends State<BottomNaviBar> {
+  late final String? titre;
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titre!),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_upward),
+            label: 'Progression',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Communaute',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Cours',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
