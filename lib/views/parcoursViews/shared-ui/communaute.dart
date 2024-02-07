96d8c2aeaf650677c0c37c04813552a2ce4486d1
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-07 03:14:24 +0000

    creation du page html

---
+++ lib/views/parcoursViews/shared-ui/communaute.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Communaute extends StatefulWidget {
+  const Communaute({super.key});
+
+  @override
+  State<Communaute> createState() => _CommunauteState();
+}
+
+class _CommunauteState extends State<Communaute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Communaute'),
+      ),
+    );
+  }
+}
