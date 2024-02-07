049b93a753a385d8370a409a2b977e0e577fa4c0
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-07 02:59:06 +0000

    creation du bottomNavigation

--- lib/views/parcoursViews/html.dart
+++ lib/views/parcoursViews/html.dart
@@ -1,5 +1,7 @@
 import 'package:code_crafters/views/parcoursViews/shared-ui/bottomNaviagtionBar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HTMLView extends StatefulWidget {
   const HTMLView({super.key});
@@ -9,8 +11,23 @@ class HTMLView extends StatefulWidget {
 }
 
 class _HTMLViewState extends State<HTMLView> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return BottomNaviBar();
+     final _user = Provider.of<User?>(context);
+    return Scaffold(
+      appBar:  AppBar(
+        title: Text('Parcours HTML'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.grey,
+              backgroundImage: NetworkImage(_user!.photoURL!),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
