d6905fc1e59648723bb93801759a4b651b893c6f
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-08 04:23:49 +0000

    creation de la fonction pour ajouter l'id  dsns le cloud

--- lib/services/cours_Id.dart
+++ lib/services/cours_Id.dart
@@ -1,16 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// une fonction pour mettre à jour l'ID du conteneur sélectionné dans Firestore
+// une fonction pour mettre à jour l'ID du conteneur sélectionné dans Firestore 
 
 Future<void> updateSelectedContainerId(User user, String containerId) async {
-  final userDocRef =
-      FirebaseFirestore.instance.collection('users').doc(user.uid);
-  await userDocRef.set(
-      {
-        'selectedContainerId': containerId,
-      },
-      SetOptions(
-          merge:
-              true)); // Utilisez SetOptions(merge: true) pour fusionner les données sans les remplacer
+  final userDocRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+  await userDocRef.set({
+    'selectedContainerId': containerId,
+  }, SetOptions(merge: true)); // Utilisez SetOptions(merge: true) pour fusionner les données sans les remplacer
 }
+
