8480b2da22f08dc4cafdb565deaa42ec574fe27b
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-10 15:48:43 +0000

    creation du ui du chapitre1 html

--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -155,6 +155,27 @@ class _ScreenHtmlState extends State<ScreenHtml> {
                         _selectedModule.courseUnlockedStatus[index];
 
                     return GestureDetector(
+                       onTap: () {
+                        if (isUnlocked) {
+                          // Naviguer vers la page d'apprentissage du chapitre
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChapterLearningPage(
+                                chapterTitle: course,
+                                // Vous pouvez passer d'autres données pertinentes à cette page ici
+                              ),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                  'Veuillez terminer le chapitre précédent pour déverrouiller celui-ci.'),
+                            ),
+                          );
+                        }
+                      },
                       child: Column(
                         children: [
                           const SizedBox(height: 40),
