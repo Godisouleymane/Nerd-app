3704505bd513346cdf1ce5e9ac3fbb5d96fe5bc0
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-12 03:41:00 +0000

    ajout du cours html dans la base de donnees firestore

--- lib/Cours_data/html_data/html_data.dart
+++ lib/Cours_data/html_data/html_data.dart
@@ -3,19 +3,35 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Course {
   final String id;
   final String title;
-  final List<Modules> modules;
+  final List<Module> modules;
 
   Course({required this.id, required this.modules, required this.title});
 
   Map<String, dynamic> toMap() {
     return {
-      'id':id,
-      'title':title,
+      'id': id,
+      'title': title,
       'modules': modules.map((module) => module.toMap()).toList(),
     };
   }
 }
 
-class Modules {
-  
+class Module {
+  final String title;
+  final List<Chapter> chapters;
+
+  Module({
+    required this.title,
+    required this.chapters,
+  });
+
+   Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'chapters': chapters.map((chapter) => chapter.toMap()).toList(),
+    };
+  }
+}
+
+class Chapter {
 }
