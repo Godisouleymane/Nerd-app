9c5b2a2eafc488f6f4ed071299bd0b2005b08305
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-14 01:24:36 +0000

    recuperer et afficher le cours html dpuis firestore

--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -1,50 +1,88 @@
-import 'package:code_crafters/services/get_courses.dart';
 import 'package:flutter/material.dart';
+import 'package:code_crafters/services/get_courses.dart';
+import 'package:provider/provider.dart';
 
-class HtmlCourseScreen extends StatefulWidget {
-  @override
-  _HtmlCourseScreenState createState() => _HtmlCourseScreenState();
-}
-
-class _HtmlCourseScreenState extends State<HtmlCourseScreen> {
-  Course? htmlCourse;
-
-  @override
-  void initState() {
-    super.initState();
-    // Appeler la fonction pour récupérer les données du cours HTML depuis Firestore
-    getHtmlCourseFromFirestore().then((course) {
-      setState(() {
-        htmlCourse = course;
-      });
-    });
-  }
+class HtmlCourseScreen extends StatelessWidget {
+  const HtmlCourseScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<Course?>(
+      future: getHtmlCourseFromFirestore(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text('Une erreur s\'est produite: ${snapshot.error}'),
+            ),
+          );
+        } else {
+          return CourseInterface(course: snapshot.data!);
+        }
+      },
+    );
+  }
+}
+
+class CourseInterface extends StatelessWidget {
+  final Course course;
+
+  const CourseInterface({Key? key, required this.course}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firstModule = course.modules.first;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cours HTML'),
-      ),
-      body: htmlCourse == null
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: htmlCourse!.modules.length,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return ListView.builder(
+                    itemCount: course.modules.length,
+                    itemBuilder: (context, index) {
+                      final module = course.modules[index];
+                      return ListTile(
+                        title: Text(module.title),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      );
+                    },
+                  );
+                },
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                firstModule.title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: firstModule.chapters.length,
               itemBuilder: (context, index) {
-                Module module = htmlCourse!.modules[index];
-                return ExpansionTile(
-                  title: Text(module.title),
-                  children: module.chapters.map((chapter) {
-                    return ListTile(
-                      title: Text(chapter.title),
-                      onTap: () {
-                        // Ajouter la logique pour afficher les leçons du chapitre
-                      },
-                    );
-                  }).toList(),
+                final chapter = firstModule.chapters[index];
+                return ListTile(
+                  title: Text(chapter.title),
                 );
               },
             ),
+          ),
+        ],
+      ),
     );
   }
 }
