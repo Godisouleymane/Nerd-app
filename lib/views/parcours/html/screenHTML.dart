31aa7c518e5b4e87364a3e2b96b27ab0b9c8fa68
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-14 01:31:26 +0000

    recuperer et afficher le cours html dpuis firestore

--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -1,88 +1,132 @@
 import 'package:flutter/material.dart';
-import 'package:code_crafters/services/get_courses.dart';
+import 'package:code_crafters/services/get_courses.dart'; // Assurez-vous que vous importez correctement vos services
 import 'package:provider/provider.dart';
 
-class HtmlCourseScreen extends StatelessWidget {
+class HtmlCourseScreen extends StatefulWidget {
   const HtmlCourseScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Course?>(
-      future: getHtmlCourseFromFirestore(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text('Une erreur s\'est produite: ${snapshot.error}'),
-            ),
-          );
-        } else {
-          return CourseInterface(course: snapshot.data!);
-        }
-      },
-    );
-  }
+  _HtmlCourseScreenState createState() => _HtmlCourseScreenState();
 }
 
-class CourseInterface extends StatelessWidget {
-  final Course course;
+class _HtmlCourseScreenState extends State<HtmlCourseScreen> {
+  late Course? _htmlCourse;
+  late Module _selectedModule;
 
-  const CourseInterface({Key? key, required this.course}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    _loadHtmlCourse();
+  }
+
+  Future<void> _loadHtmlCourse() async {
+    final course = await getHtmlCourseFromFirestore();
+    setState(() {
+      _htmlCourse = course;
+      if (course != null && course.modules.isNotEmpty) {
+        _selectedModule = course.modules.first;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final firstModule = course.modules.first;
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return ListView.builder(
-                    itemCount: course.modules.length,
-                    itemBuilder: (context, index) {
-                      final module = course.modules[index];
-                      return ListTile(
-                        title: Text(module.title),
-                        onTap: () {
-                          Navigator.pop(context);
+      backgroundColor: Colors.grey.shade300,
+      body: _htmlCourse == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _showModulesBottomSheet(context);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.circular(16.0),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(
+                              Icons.list,
+                              size: 30,
+                            ),
+                            Flexible(
+                              child: Text(
+                                _selectedModule.title,
+                                softWrap: true,
+                                style: const TextStyle(
+                                  color: Color.fromARGB(255, 53, 32, 149),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _selectedModule.chapters.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final chapter = _selectedModule.chapters[index];
+                          return ListTile(
+                            title: Text(chapter.title),
+                            // Vous pouvez ajouter une action lorsque l'utilisateur
+                            // sélectionne un chapitre ici
+                          );
                         },
-                      );
-                    },
-                  );
-                },
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                firstModule.title,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: firstModule.chapters.length,
-              itemBuilder: (context, index) {
-                final chapter = firstModule.chapters[index];
-                return ListTile(
-                  title: Text(chapter.title),
-                );
+    );
+  }
+
+  void _showModulesBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView.builder(
+          itemCount: _htmlCourse!.modules.length,
+          itemBuilder: (BuildContext context, int index) {
+            final module = _htmlCourse!.modules[index];
+            final isSelected = module == _selectedModule;
+            return ListTile(
+              title: Text(module.title),
+              selected: isSelected,
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selectedModule = module;
+                });
               },
-            ),
-          ),
-        ],
-      ),
+            );
+          },
+        );
+      },
     );
   }
 }
