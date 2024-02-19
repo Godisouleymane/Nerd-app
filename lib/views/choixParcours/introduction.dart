3377b5e2cf914de5d67a32aed73ae2b64c034256
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-19 09:24:52 +0000

    ajout de la progression de l'utilisateur dans firestore

--- lib/views/choixParcours/introduction.dart
+++ lib/views/choixParcours/introduction.dart
@@ -259,7 +259,11 @@ class _IntroductionPageState extends State<IntroductionPage> {
 
             final user = Provider.of<User?>(context, listen: false);
 
-           
+           if (user != null) {
+             FirebaseFirestore.instance.collection('users')
+           } else {
+             
+           }
           },
           child: const Text(
             "Commencer",
