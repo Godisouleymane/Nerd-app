164bf7e83c57f575dd8f12ec07331fb24fdad3a8
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-26 23:11:12 +0100

    creation de l'interface profile

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -15,33 +15,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final _user = Provider.of<User?>(context);
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.30,
-              decoration: const BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40), bottomRight: Radius.circular(40))
-              ),
-              child: Center(
-                child: Text(_user!.displayName!,style: TextStyle(
-                  color: Colors.white
-                ),),
-              ),
-            ),
-            Positioned(
-             bottom: 0,
-             right: 0,
-             left: 0,
-              child: Center(
-                child: CircleAvatar(
-                radius: 40,
-                backgroundColor: Colors.grey,
-                backgroundImage: NetworkImage(_user!.photoURL!),
-                            ),
-              ))
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  decoration: const BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30))
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(_user!.photoURL!),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Center(
+                        child: Text(_user!.displayName!,style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),),
+                      ),
+                    ],
+                  ),
+                ),  
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Activités', style: TextStyle(
+                 fontSize: 18
+              ),),
+            )
           ],
         ),
+        
       ),
     );
   }
