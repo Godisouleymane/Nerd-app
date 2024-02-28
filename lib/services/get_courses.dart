8367a2878d96d182678866655a0cc1ae7a92d57c
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-29 00:13:13 +0100

    recupeeration des donnees html

--- lib/services/get_courses.dart
+++ lib/services/get_courses.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 Future<Map<String, dynamic>?> getHtmlCourseFromFirestore() async {
   DocumentSnapshot snapshot =
       await FirebaseFirestore.instance.collection('courses').doc('html').get();
-
   if (snapshot.exists) {
     return snapshot.data() as Map<String, dynamic>?;
   }
--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -1,4 +1,3 @@
-
 import 'package:code_crafters/services/get_courses.dart';
 import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart';
@@ -14,70 +13,69 @@ class _HtmlViewwState extends State<HtmlVieww> {
   @override
   void initState() {
     super.initState();
-    if (courseFuture == null) {
-      courseFuture =
-          getHtmlCourseFromFirestore() as Future<Map<String, dynamic>?>?;
-    }
+    courseFuture = getHtmlCourseFromFirestore();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: FutureBuilder<Map<String, dynamic>?>(
+      body: FutureBuilder<Map<String, dynamic>?>(
         future: courseFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasData) {
               final courseData = snapshot.data!;
-              // Utilisez une liste vide comme valeur par défaut si 'modules' est null
               final modules = courseData['modules'] as List<dynamic>? ?? [];
               return ListView.builder(
                 itemCount: modules.length,
                 itemBuilder: (context, index) {
                   final module = modules[index] as Map<String, dynamic>;
-                  // Utilisez une liste vide comme valeur par défaut si 'lessons' est null
-                  final lessons = module['lessons'] as List<dynamic>? ?? [];
-                  return Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TimelineTile(
-                      alignment: TimelineAlign.start,
-                      lineXY: 0.3,
-                      isFirst: index == 0,
-                      isLast: index == modules.length - 1,
-                      indicatorStyle: IndicatorStyle(
-                        width: 40,
-                        color: module['isUnlocked'] ? Colors.green : Colors.grey,
-                        iconStyle: IconStyle(
-                          iconData: module['isUnlocked']
-                              ? Icons.play_arrow
-                              : Icons.lock,
-                          color: Colors.white,
+                 final lessons = module['lesson'] as List<dynamic>? ?? [];
+                  return SizedBox(
+                    height: 200,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TimelineTile(
+                        alignment: TimelineAlign.start,
+                        lineXY: 0.3,
+                        isFirst: index == 0,
+                        isLast: index == modules.length - 1,
+                        
+                        indicatorStyle: IndicatorStyle(
+                          width: 40,
+                          color:
+                              module['isUnlocked'] ? Colors.green : Colors.grey,
+                          iconStyle: IconStyle(
+                            iconData: module['isUnlocked']
+                                ? Icons.play_arrow
+                                : Icons.lock,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      endChild: ExpansionTile(
-                        title: Text(module['title']),
-                        children: lessons.map((lesson) {
-                          final lessonData = lesson as Map<String, dynamic>;
-                          return ListTile(
-                            title: Text(lessonData['title']),
-                            subtitle: Text(lessonData['content']),
-                          );
-                        }).toList(),
+                        endChild: ExpansionTile(
+                          title: Text(module['title']),
+                          children: lessons.map<Widget>((lesson) {
+                            final lessonData = lesson as Map<String, dynamic>;
+                            return ListTile(
+                              title: Text(lessonData['title']),
+                              subtitle: Text(lessonData['content']),
+                            );
+                          }).toList(),
+                        ),
                       ),
                     ),
                   );
                 },
               );
-            } else if (snapshot.hasError) {
-              return Center(
-                  child: Text('Erreur lors du chargement des données'));
+            } else {
+              return Center(child: Text('Aucune donnée de cours disponible.'));
             }
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erreur lors du chargement des données'));
           }
           return Center(child: CircularProgressIndicator());
         },
       ),
-
     );
   }
 }
