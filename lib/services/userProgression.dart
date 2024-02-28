242d37edef19239f6a16fc004ed9e28fc7e04d1a
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-28 16:07:41 +0100

    fonction pour la progression de l'utilisateur

--- lib/services/userProgression.dart
+++ lib/services/userProgression.dart
@@ -1,3 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+// Fonction pour initialiser la progression de l'utilisateur
+
+void initializeHTMLCourseProgressForUser() async{
+  
+}
