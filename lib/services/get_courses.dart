eb86d18b7bbb96b5136fc59c9fd99814bdd27d42
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-28 23:30:40 +0100

    recupeeration des donnees html

--- lib/services/get_courses.dart
+++ lib/services/get_courses.dart
@@ -1,21 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<Map<String, dynamic>?> getHtmlCourseFromFirestore() async {
-  try {
-    DocumentSnapshot courseSnapshot = await FirebaseFirestore.instance
-        .collection('courses')
-        .doc('html')
-        .get();
 
-    if (courseSnapshot.exists) {
-      return courseSnapshot.data() as Map<String, dynamic>;
-    } else {
-      print('Le cours HTML n\'a pas été trouvé dans Firestore');
-      return null;
-    }
-  } catch (error) {
-    print(
-        'Erreur lors de la récupération du cours HTML depuis Firestore: $error');
-    return null;
+Future<Map<String, dynamic>?> getHtmlCourseFromFirestore() async {
+  DocumentSnapshot snapshot =
+      await FirebaseFirestore.instance.collection('courses').doc('html').get();
+
+  if (snapshot.exists) {
+    return snapshot.data() as Map<String, dynamic>?;
   }
+  return null;
 }
--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -1,3 +1,4 @@
+
 import 'package:code_crafters/services/get_courses.dart';
 import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart';
@@ -13,51 +14,57 @@ class _HtmlViewwState extends State<HtmlVieww> {
   @override
   void initState() {
     super.initState();
-    courseFuture = getHtmlCourseFromFirestore();
+    if (courseFuture == null) {
+      courseFuture =
+          getHtmlCourseFromFirestore() as Future<Map<String, dynamic>?>?;
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cours HTML'),
-      ),
-      body: FutureBuilder<Map<String, dynamic>?>(
+     body: FutureBuilder<Map<String, dynamic>?>(
         future: courseFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasData) {
               final courseData = snapshot.data!;
-              final modules = courseData['modules'] as List<dynamic>;
+              // Utilisez une liste vide comme valeur par défaut si 'modules' est null
+              final modules = courseData['modules'] as List<dynamic>? ?? [];
               return ListView.builder(
                 itemCount: modules.length,
                 itemBuilder: (context, index) {
                   final module = modules[index] as Map<String, dynamic>;
-                  final lessons = module['lessons'] as List<dynamic>;
-                  return TimelineTile(
-                    alignment: TimelineAlign.manual,
-                    lineXY: 0.3,
-                    isFirst: index == 0,
-                    isLast: index == modules.length - 1,
-                    indicatorStyle: IndicatorStyle(
-                      width: 40,
-                      color: module['isUnlocked'] ? Colors.green : Colors.grey,
-                      iconStyle: IconStyle(
-                        iconData: module['isUnlocked']
-                            ? Icons.play_arrow
-                            : Icons.lock,
-                        color: Colors.white,
+                  // Utilisez une liste vide comme valeur par défaut si 'lessons' est null
+                  final lessons = module['lessons'] as List<dynamic>? ?? [];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TimelineTile(
+                      alignment: TimelineAlign.start,
+                      lineXY: 0.3,
+                      isFirst: index == 0,
+                      isLast: index == modules.length - 1,
+                      indicatorStyle: IndicatorStyle(
+                        width: 40,
+                        color: module['isUnlocked'] ? Colors.green : Colors.grey,
+                        iconStyle: IconStyle(
+                          iconData: module['isUnlocked']
+                              ? Icons.play_arrow
+                              : Icons.lock,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    endChild: ExpansionTile(
-                      title: Text(module['title']),
-                      children: lessons.map((lesson) {
-                        final lessonData = lesson as Map<String, dynamic>;
-                        return ListTile(
-                          title: Text(lessonData['title']),
-                          subtitle: Text(lessonData['content']),
-                        );
-                      }).toList(),
+                      endChild: ExpansionTile(
+                        title: Text(module['title']),
+                        children: lessons.map((lesson) {
+                          final lessonData = lesson as Map<String, dynamic>;
+                          return ListTile(
+                            title: Text(lessonData['title']),
+                            subtitle: Text(lessonData['content']),
+                          );
+                        }).toList(),
+                      ),
                     ),
                   );
                 },
@@ -70,6 +77,7 @@ class _HtmlViewwState extends State<HtmlVieww> {
           return Center(child: CircularProgressIndicator());
         },
       ),
+
     );
   }
 }
