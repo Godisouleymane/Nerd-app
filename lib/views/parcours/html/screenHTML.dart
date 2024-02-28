891504590c61b9bc8121127c1efd4dd682541e9b
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-29 00:25:00 +0100

    creation de l'interface htmlView avec les donnees recuperer

--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -26,7 +26,51 @@ class _HtmlViewwState extends State<HtmlVieww> {
             if (snapshot.hasData) {
               final courseData = snapshot.data!;
               final modules = courseData['modules'] as List<dynamic>? ?? [];
-              
+              return ListView.builder(
+                itemCount: modules.length,
+                itemBuilder: (context, index) {
+                  final module = modules[index] as Map<String, dynamic>;
+                  final lessons = module['lesson'] as List<dynamic>? ?? [];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TimelineTile(
+                      alignment: TimelineAlign.start,
+                      lineXY: 0.3,
+                      isFirst: index == 0,
+                      isLast: index == modules.length - 1,
+                      indicatorStyle: IndicatorStyle(
+                        width: 40,
+                        color:
+                            module['isUnlocked'] ? Colors.green : Colors.grey,
+                        iconStyle: IconStyle(
+                          iconData: module['isUnlocked']
+                              ? Icons.play_arrow
+                              : Icons.lock,
+                          color: Colors.white,
+                        ),
+                      ),
+                      endChild: ExpansionTile(
+                        title: Text(module['title']),
+                        children: <Widget>[
+                          SingleChildScrollView(
+                            child: Column(
+                              children: lessons.map<Widget>((lesson) {
+                                final lessonData =
+                                    lesson as Map<String, dynamic>;
+                                return ListTile(
+                                  title: Text(lessonData['title']),
+                                  subtitle: Text(lessonData['content']),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+
             } else {
               return Center(child: Text('Aucune donnée de cours disponible.'));
             }
