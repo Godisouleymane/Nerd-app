e567d356d59b7c6433146f58fcec8b4674641341
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-29 03:08:21 +0100

    creation de l'interface htmlView avec les donnees recuperer

--- lib/views/parcours/html/screenHTML.dart
+++ lib/views/parcours/html/screenHTML.dart
@@ -1,4 +1,5 @@
 import 'package:code_crafters/services/get_courses.dart';
+import 'package:dev_icons/dev_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
@@ -58,7 +59,7 @@ class _HtmlViewwState extends State<HtmlVieww> {
                       itemCount: lessons.length,
                       itemBuilder: (context, index) {
                         return Container(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
                           height: 200,
                           child: TimelineTile(
                             isFirst: index == 0,
@@ -66,7 +67,7 @@ class _HtmlViewwState extends State<HtmlVieww> {
                             alignment: TimelineAlign.start,
                             lineXY: 0.1,
                             beforeLineStyle: const LineStyle(
-                                thickness: 4, color: Colors.black),
+                                thickness: 4, color: Colors.deepPurple),
                             indicatorStyle: IndicatorStyle(
                               width: 35,
                               color: Colors.grey,
@@ -74,23 +75,36 @@ class _HtmlViewwState extends State<HtmlVieww> {
                                   iconData: Icons.lock, color: Colors.white),
                             ),
                             endChild: Padding(
-                                padding: EdgeInsets.all(8.0),
+                                padding: const EdgeInsets.all(8.0),
                                 child: Container(
-                                  padding: EdgeInsets.all(40),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                   borderRadius: BorderRadius.circular(10),
-                                   boxShadow: const [
-                                    BoxShadow(
-                                      blurRadius: 2,
-                                      offset: Offset.zero
-                                    )
-                                   ]
-                                  ),
-                                  child: Text(
-                                    lessons[index]['title'],
-                                  ),
-                                )),
+                                    height: MediaQuery.of(context).size.height *
+                                        0.11,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: const [
+                                          BoxShadow(
+                                            color: Colors.grey,
+                                            spreadRadius: 2,
+                                            blurRadius: 0,
+                                            offset: Offset(0, 2),
+                                          )
+                                        ]),
+                                    child: ListTile(
+                                      leading: Icon(DevIcons.html5Plain,
+                                          color: Colors.orange),
+                                      trailing: Icon(
+                                        Icons.book_rounded,
+                                        color: Colors.deepPurple,
+                                      ),
+                                      title: Text(
+                                        lessons[index]['title'],
+                                      ),
+                                      titleTextStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ))),
                           ),
                         );
                       },
@@ -99,10 +113,12 @@ class _HtmlViewwState extends State<HtmlVieww> {
                 ],
               );
             } else {
-              return const Center(child: Text('Aucune donnée de cours disponible.'));
+              return const Center(
+                  child: Text('Aucune donnée de cours disponible.'));
             }
           } else if (snapshot.hasError) {
-            return const Center(child: Text('Erreur lors du chargement des données'));
+            return const Center(
+                child: Text('Erreur lors du chargement des données'));
           }
           return const Center(child: CircularProgressIndicator());
         },
