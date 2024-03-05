d89041900013459296d613858761474a14256e52
GodiSouleymane <ssabiougodi@gmail.com>
2024-03-05 15:52:06 +0100

    creation du login page

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -18,44 +19,151 @@ class LoginPage extends StatelessWidget {
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(30),
                         bottomRight: Radius.circular(30))),
-                        child: const Center(
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage('assets/logo.webp'),
-                            radius: 70,
-                            )
-                        ),
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundImage: const AssetImage('assets/logo.webp'),
+                    backgroundColor: Colors.grey.shade300,
+                    radius: 70,
+                  ),
+                ),
               ),
             ],
           ),
           Positioned(
-            top: MediaQuery.of(context).size.height *
-                0.4, // Ajustez cette valeur pour déplacer le conteneur blanc vers le haut
+            top: MediaQuery.of(context).size.height * 0.4, // Ajustement
             left: 0,
             right: 0,
             child: Container(
-              height: MediaQuery.of(context).size.height,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
+              height:
+                  MediaQuery.of(context).size.height, // Hauteur ajustée
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(30),
                     topRight: Radius.circular(30)),
               ),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text('Authentification', style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold
-                      ),),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Authentification',
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                  ),
-                  
-                ],
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Authentifiez-vous pour sauvegarder et recuperer votre progression.',
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+
+                   Padding(
+                     padding: const EdgeInsets.all(20.0),
+                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        elevation: 8.0,
+                        minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      onPressed: (){}, 
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(MdiIcons.facebook, color: Colors.white, size: 25,),
+                          const Text('Se connecter avec facebook', style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15
+                          ),)],
+                      ),
+                      ),
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(20.0),
+                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        elevation: 8.0,
+                        minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      onPressed: (){}, 
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(MdiIcons.google, color: Colors.red, size: 25,),
+                          const Text('Se connecter avec google', style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 15 
+                          ),)],
+                      ),
+                      ),
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(20.0),
+                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 64, 41, 41),
+                        elevation: 8.0,
+                        minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      onPressed: (){}, 
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(MdiIcons.github, color: Colors.white, size: 25,),
+                          const Text('Se connecter avec github', style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15
+                          ),)],
+                      ),
+                      ),
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(20.0),
+                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        elevation: 8.0,
+                        minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      onPressed: (){}, 
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(MdiIcons.apple, color: Colors.white, size: 25,),
+                          const Text('Se connecter avec apple', style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15
+                          ),)],
+                      ),
+                      ),
+                   ),
+                  ],
+                ),
               ),
-              
             ),
           ),
         ],
