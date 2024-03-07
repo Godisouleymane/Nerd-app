e30122cd5c2a1dd84e21d94fc467bb42ae4f536e
GodiSouleymane <ssabiougodi@gmail.com>
2024-03-07 05:37:00 +0100

    ajout du header dans l'introduction page

--- lib/views/choixParcours/introduction.dart
+++ lib/views/choixParcours/introduction.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-// Assurez-vous d'ajouter fluttertoast à votre pubspec.yaml
 
 class IntroductionPage extends StatefulWidget {
   const IntroductionPage({Key? key}) : super(key: key);
@@ -13,9 +12,9 @@ class IntroductionPage extends StatefulWidget {
 
 class _IntroductionPageState extends State<IntroductionPage> {
   String? selectedCourseId;
+
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -35,69 +34,87 @@ class _IntroductionPageState extends State<IntroductionPage> {
           }
           if (!snapshot.hasData) {
             return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.teal,
+                child: CircularProgressIndicator(color: Colors.teal));
+          }
+
+          // Widget En-tête
+          Widget header = Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Bienvenue dans votre sélection de cours",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          );
+
+          // Widgets des Cours
+          List<Widget> courseWidgets =
+              snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            bool isSelected = data['id'] == selectedCourseId;
+
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedCourseId = data['id'];
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: isSelected ? Colors.teal : Colors.blueGrey,
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.network(
+                          data['imageUrl'],
+                          width: 90,
+                          height: 90,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                              left: 10, right: 10.0, bottom: 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                data['titre'],
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                data['description'],
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             );
-          }
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              bool isSelected = data['id'] ==
-                  selectedCourseId; 
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedCourseId = data['id'];
-                    print(selectedCourseId);
-                  });
-                },
-                child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Card(
-                        color: isSelected ? Colors.teal : Colors.blueGrey,
-                        elevation: 5,
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SvgPicture.network(
-                                data['imageUrl'],
-                                width: 90,
-                                height: 90,
-                              ),
-                            ),
-                            Expanded(
-                                child: Container(
-                              padding: const EdgeInsets.only(left: 10, right: 10.0, bottom: 8.0),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      data['titre'],
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                    Text(
-                                      data['description'],
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.white
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 5,
-                                    ),
-                                  ]),
-                            )),
-                          ],
-                        ))),
-              );
-            }).toList(),
+          }).toList();
+
+          // Construction de la liste avec un En-tête
+          return SingleChildScrollView(
+            child: Column(
+              children: [header] + courseWidgets,
+            ),
           );
         },
       ),
@@ -105,22 +122,21 @@ class _IntroductionPageState extends State<IntroductionPage> {
         padding: const EdgeInsets.only(right: 15.0, left: 15.0, bottom: 15.0),
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.teal,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              minimumSize: const Size(double.infinity, 50.0),
-              elevation: 10),
-         onPressed: () {
+            backgroundColor: Colors.teal,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            minimumSize: const Size(double.infinity, 50.0),
+            elevation: 10,
+          ),
+          onPressed: () {
             if (selectedCourseId != null) {
-              // Exemple : Affichez un toast indiquant le cours sélectionné
               Fluttertoast.showToast(
                 msg: "Cours sélectionné : $selectedCourseId",
                 toastLength: Toast.LENGTH_SHORT,
                 gravity: ToastGravity.BOTTOM,
               );
-              // Ajoutez ici votre logique pour naviguer vers la page du cours
+              // Naviguer vers la page du cours ou une autre action ici
             } else {
-              // Si aucun cours n'est sélectionné, affichez un message
               Fluttertoast.showToast(
                 msg: "Veuillez sélectionner un cours.",
                 toastLength: Toast.LENGTH_SHORT,
