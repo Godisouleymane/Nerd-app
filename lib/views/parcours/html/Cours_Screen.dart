f7992641a497050778355d03d816c35ca69697bf
GodiSouleymane <ssabiougodi@gmail.com>
2024-03-14 15:38:21 +0100

    recuperation et affichage du module depuis firestore

--- lib/views/parcours/html/Cours_Screen.dart
+++ lib/views/parcours/html/Cours_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:code_crafters/views/widgets/showSnackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:lottie/lottie.dart';
@@ -38,28 +39,55 @@ class _CourseScreenState extends State<CourseScreen> {
               padding: const EdgeInsets.all(10.0),
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                decoration:  BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      blurRadius: 3,
-                      spreadRadius: 3,
-                      offset: const Offset(0, 3)
-                    )
-                  ]
-                ),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 3,
+                          spreadRadius: 3,
+                          offset: const Offset(0, 3))
+                    ]),
                 child: Column(
                   children: [
-                    Image.asset('assets/vector.png'),
+                    GestureDetector(
+                        onTap: () => data['estDebloquer']
+                            ? showNotification(
+                                context, 'ce module est debloquer')
+                            : showNotification(context,
+                                'Veuillez terminer le module presedent pour le debloquer'),
+                        child: Image.asset('assets/vector.png')),
                     const Gap(8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                      Text(data['titre']),
-                      Icon(data['estDebloquer'] ? Icons.play_arrow : Icons.lock, size: 30, color: data['estDebloquer'] ? Colors.teal : Colors.red)
-                    ],)
+                    Container(
+                      decoration: const BoxDecoration(
+                          color: Colors.teal,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10))),
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 5.0, left: 5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              data['titre'],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                                data['estDebloquer']
+                                    ? Icons.play_arrow
+                                    : Icons.lock,
+                                size: MediaQuery.of(context).size.width * 0.13,
+                                color: data['estDebloquer']
+                                    ? Colors.green
+                                    : Colors.red)
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
